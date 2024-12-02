11d2099474cb9339fbe976e003b0b1cb085fdcc4
Mebo <samkharadzemerab@gmail.com>
2024-12-02 22:25:31 +0400

    move files, change layouts, add lang button on header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import React from "react";
+
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500"],
@@ -13,17 +19,26 @@ export const metadata: Metadata = {
   description: "Give Wings to Your Talent",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+  params,
+}: {
   children: React.ReactNode;
-}>) {
+  params: { locale: string };
+}) {
+  const { locale } = await params;
+
+  const messages = await getMessages();
   return (
-    <html lang="en" className={inter.className}>
-      <body>
-        <Header />
-        {children}
-      </body>
+    <html lang={locale} className={inter.className}>
+      <UserProvider>
+        <body>
+          <NextIntlClientProvider messages={messages}>
+            <Header />
+            {children}
+          </NextIntlClientProvider>
+        </body>
+      </UserProvider>
     </html>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import { Link } from "@/i18n/routing";
+import { useRouter, usePathname } from "next/navigation";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const changeLanguage = (locale: string) => {
+    setIsOpen(false);
+
+    router.push(`/${locale}${pathname.slice(3)}`);
+  };
+  const { user } = useUser();
   return (
     <header className="bg-blue-600 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
@@ -16,8 +30,36 @@ const Header = () => {
           <Link href="/products" className="hover:text-gray-200 transition">
             Products
           </Link>
-          <a href="/api/auth/login">Login</a>
+          {user ? (
+            <a href="/api/auth/logout">Logout</a>
+          ) : (
+            <a href="/api/auth/login">Login</a>
+          )}
         </nav>
+        <div className="relative">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="bg-black text-white p-2 rounded-md border text-sm border-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            {pathname.includes("/ka") ? "ქარ" : "Eng"}
+          </button>
+          {isOpen && (
+            <div className="absolute bg-white text-black shadow-lg rounded-md mt-2 w-40 overflow-hidden">
+              <button
+                onClick={() => changeLanguage("en")}
+                className="w-full text-left px-4 py-2 hover:bg-gray-200"
+              >
+                English
+              </button>
+              <button
+                onClick={() => changeLanguage("ka")}
+                className="w-full text-left px-4 py-2 hover:bg-gray-200"
+              >
+                ქართული
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );

88519e0ce8ffffbe70eec57e6b9841cd0f4ed86b
Mebo <samkharadzemerab@gmail.com>
2024-12-02 19:44:10 +0400

    add internationalisation by next-intl doc

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "../../i18n/routing";
+import React from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+export const metadata: Metadata = {
+  title: "Handmade",
+  description: "Give Wings to Your Talent",
+};
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as "en" | "ka")) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} className={inter.className}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
